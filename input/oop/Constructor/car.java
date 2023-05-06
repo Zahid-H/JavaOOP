292ed58734749d716fb6caa84c9e03a789e6bb04
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-05-06 19:53:48 +0600

    renamed folder

---
+++ input/oop/Constructor/car.java
@@ -0,0 +1,15 @@
+package input.oop.Constructor;
+
+public class car {
+    String model;
+    int serial;
+    String color;
+
+    car() {
+
+    }
+
+    void getinfo() {
+        System.out.println("Model:" + model + "Serial:" + serial + "Color:" + color);
+    }
+}
