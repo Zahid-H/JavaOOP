8f1b2d3558385859aea733e458cc5b00d27b405b
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-05-06 14:06:28 +0600

    Re practicing java array with for each loop

---
+++ input/student.java
@@ -0,0 +1,10 @@
+package input;
+
+public class student {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 6 };
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
