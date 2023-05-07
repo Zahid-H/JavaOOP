80145133db6229dbe6402a61c27be1d8e47a0395
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-05-07 15:22:37 +0600

    PRacticing Encapsulation

---
+++ input/oop/Encapsulation/Person.java
@@ -0,0 +1,15 @@
+package input.oop.Encapsulation;
+
+public class Person {
+    private String name;
+    private String age;
+
+    // method
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ input/oop/Encapsulation/TestPerson.java
@@ -0,0 +1,9 @@
+package input.oop.Encapsulation;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("Elon Mask");
+        System.out.println(p.getName());
+    }
+}
