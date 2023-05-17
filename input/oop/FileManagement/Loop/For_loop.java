06385867f0694fe139ac2763d433597e6363b21b
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-05-17 21:09:32 +0600

    practicing for loop

---
+++ input/oop/FileManagement/Loop/For_loop.java
@@ -0,0 +1,10 @@
+package input.oop.FileManagement.Loop;
+
+public class For_loop {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i <= n; i += 4) {
+            System.out.println(i);
+        }
+    }
+}
